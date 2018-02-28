bde398c9a1e6af601bfacd5d6fa0963852b68f7a
haiwennnn <haiwengoodboy@126.com>
2018-03-01 01:39:31 +0800

    增加gulp处理less

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+  less = require('gulp-less'),
+  path = require('path'),
+  autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('v2', function () {
+  return gulp.src('./example/v2/style/*.less')
+    .pipe(less({
+      paths: [path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(autoprefixer({
+      browsers: ['> 1%', 'last 2 versions', 'not ie <= 8'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./example/v2/style'));
+})
+
+gulp.task('v1', function () {
+  return gulp.src('./example/v1/style/*.less')
+    .pipe(less({
+      paths: [path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 4 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./example/v1/style'));
+})
+
+gulp.task('dev', ['v1', 'v2'], function () {
+  var watcher = gulp.watch('./src/**/*.less', ['v1', 'v2']);
+  watcher.on('change', function (event) {
+    // console.log(event)
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+})
